1224fcf1b545e3d0321d81f9177a508022923942
KAUANSANTOS <kauanauan71@gmail.com>
2021-10-26 11:26:28 -0300

    Atualizações calculadora

---
+++ calculadora-gorjeta/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './App.css';
+
+function App() {
+  let nome ='kauan'
+  let idade= 19
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+        <a>
+        <h6>testando Vou ser um otimo Programador Front-end Deus vai me Honrar SOU O {nome} e teho {idade} anos</h6>
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
