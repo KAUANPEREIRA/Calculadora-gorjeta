56107bfb31786c4b0f11b2f4975665649c2e3df8
KAUANSANTOS <kauanauan71@gmail.com>
2021-10-27 15:46:35 -0300

    atualizações na calculadora de porcetagem

--- calculadora-gorjeta/src/App.js
+++ calculadora-gorjeta/src/App.js
@@ -1,18 +1,24 @@
 import React from 'react'
 import './App.css';
+import styled from 'styled-components'
+
+const Site = styled.div`
+
+`
+const Botao = styled.button`
+font-size:19px;
+padding:15px 10px;
+
+`
 
 function App() {
-  let nome ='kauan'
-  let idade= 19
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-      
-        <a>
-        <h6>testando Vou ser um otimo Programador Front-end Deus vai me Honrar SOU O {nome} e teho {idade} anos</h6>
-        </a>
-      </header>
-    </div>
+    <Site>
+      <Botao>Clique aqui</Botao>
+    </Site>
+  
+    
   );
 }
 
