b91ad515ca8d42502d91291a43124f25d958f57c
KAUANSANTOS <kauanauan71@gmail.com>
2021-10-30 13:26:26 -0300

    atualização simples
    Calculadora de gorjeta

--- calculadora-gorjeta/src/App.js
+++ calculadora-gorjeta/src/App.js
@@ -1,25 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './App.css';
 import styled from 'styled-components'
 
-const Site = styled.div`
-
-`
-const Botao = styled.button`
-font-size:19px;
-padding:15px 10px;
-
-`
+//valorAlt função para pegar o valor do meu campo input...
 
 function App() {
+
+  const[valor, setValor]=useState()
+  const[porcetagem, setPorcentagem]=useState()
+
+  const valorAlt= (e)=>{
+    setValor(e.target.value)
+  }
+
+  const porcentagemAlt = (e)=>{
+    setPorcentagem(e.target.value)
+  }
+
  
+
   return (
-    <Site>
-      <Botao>Clique aqui</Botao>
-    </Site>
-  
+   <div>
+     <h3>Calculadora de gorjeta</h3>
+
+     <p>Quanto deu a Conta ?</p>
+
+     <input type={Number} value={valor} onChange={valorAlt}/><br>
+     </br>
+
+     <p>Qual há porcentagem de gorjeta?</p>
+     <input type={Number} value={porcetagem} onChange={porcentagemAlt}/><br></br>
+       <div className="resutado">
+         <p>Total R$ : {valor}</p>
+         <p>Gorjeta {porcetagem}%</p>
+         <p>Total á Pagar {parseInt(valor) + parseInt((porcetagem/100)*valor)}</p>
+
+
+         
+        
+       </div>
+   </div>
     
-  );
+  )
 }
 
 export default App;
